5d81dc62e67f5e6121030705c80872f91007507b
sytha <sytha38@gmail.com>
2014-07-03 11:16:05 +0200

    Ajout des tables SQL

---
+++ TablesSQL.sql
@@ -0,0 +1,25 @@
+CREATE TABLE INTERETS (
+id integer NOT NULL,
+libelle VARCHAR(255), 
+primary KEY (id)
+);
+
+CREATE TABLE GEEKS (
+id integer NOT NULL,
+nom VARCHAR(255),
+prenom VARCHAR(255),
+ville VARCHAR(255),
+interet integer,
+mail VARCHAR(255),
+gravatar VARCHAR(255),
+primary KEY (id),
+FOREIGN KEY (interet) REFERENCES INTERETS(id)
+);
+
+CREATE TABLE HISTORY (
+id integer NOT NULL,
+date DATE,
+geek integer,
+primary KEY (id),
+FOREIGN KEY (geek) REFERENCES GEEKS(id)
+);
