615577bd4c0eb4277a38fd81dc84afbab2a82d63
sytha <sytha38@gmail.com>
2014-07-04 11:01:47 +0200

    Modification daoGeeks car les interets étaient récupérés en lazy.

--- src/main/java/com/ninja_squad/geektic/dao/GeekDao.java
+++ src/main/java/com/ninja_squad/geektic/dao/GeekDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.*;
 
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Repository;
 
 import com.ninja_squad.geektic.*;
@@ -16,24 +17,39 @@ public class GeekDao {
 
 	
 	public List<Geek> findAll(){
-		String jpql = "SELECT g "
-				+ "FROM Geek g";
+		String jpql = "SELECT distinct g "
+				+ "FROM Geek g "
+				+ "LEFT JOIN FETCH g.interets i";
 		TypedQuery<Geek> query = em.createQuery(jpql, Geek.class); 
 		return query.getResultList();
 	}
 	
 	public Geek findById(Long id){
-		return em.find(Geek.class, id);
+		Geek g = em.find(Geek.class, id);
+		Hibernate.initialize(g.getInterets());
+		return g;
 	}
 	
-	public List<Geek> findByCriteria(String sexe, String interet){
-		String jpql = "SELECT g "
+	public List<Geek> findBySexeInteret(String sexe, String interet){
+		String jpql = "SELECT distinct g "
 				+ "FROM Geek g "
+				+ "LEFT JOIN FETCH g.interets i "
 				+ "JOIN g.interets i "
 				+ "WHERE g.sexe = :sexe "
 				+ "AND i.libelle = :interet ";
 		TypedQuery<Geek> query = em.createQuery(jpql, Geek.class); 
 		query.setParameter("sexe", sexe);
+		query.setParameter("interet", interet);
+		return query.getResultList();
+	}
+	
+	public List<Geek> findByInteret(String interet){
+		String jpql = "SELECT distinct g "
+				+ "FROM Geek g "
+				+ "LEFT JOIN FETCH g.interets i "
+				+ "JOIN g.interets i "
+				+ "WHERE i.libelle = :interet ";
+		TypedQuery<Geek> query = em.createQuery(jpql, Geek.class); 
 		query.setParameter("interet", interet);
 		return query.getResultList();
 	}
